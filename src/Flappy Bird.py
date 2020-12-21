c67792a84dbfa4afaaa917fe5ec1b8b371c0fe77
Mark21-03 <72521279+Mark21-03@users.noreply.github.com>
2020-12-21 19:58:49 +0000

    Grande Atualização
    Adição de várias skins para fundo, tubos e bird.
    A janela aparece agora centrada no ecrã e o tamanho da janela ajusta-se ao tamanho de ecrã.
    Pequenas alterações no código
    Adicionado Icon na janela do jogo.

--- src/Flappy Bird.py
+++ src/Flappy Bird.py
@@ -1,35 +1,43 @@
 import pygame
 from pygame.locals import *
 from random import randint
-from pyautogui import alert
-import tkinter
 from random import randint as rand
-import sys
 import os
+import sys
+from win32api import GetSystemMetrics
+
 
 def fps(x):pygame.time.Clock().tick(x)
 
 def getdir(f_name):return os.path.join(os.path.dirname(__file__), f_name)
-       
+    
+#Make window centered
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+#icon jogo
+pygame.display.set_icon(pygame.image.load(getdir('icon.png')))
+
 def main():
 	#Constantes
-	WIN_WIDTH=600
-	WIN_HEIGHT=900
+	SCREEN_WIDTH = GetSystemMetrics(0)
+	SCREEN_HEIGHT = GetSystemMetrics(1)
+	WIN_WIDTH=int(SCREEN_HEIGHT*0.6)
+	WIN_HEIGHT=int(SCREEN_HEIGHT*0.9)
 	PIPE_WIDTH=120
 	PIPE_HEIGHT=460
 	GAP=650
-	MOVE= 20
+	MOVE= 15
 
 	#Load de skins
 	pygame.init()
-	bird_skin=pygame.transform.scale(pygame.image.load(getdir('bird.png')),(65,60))	
-	pipe_b_skin = pygame.transform.scale(pygame.image.load(getdir('pipe.png')),(PIPE_WIDTH,PIPE_HEIGHT))
-	sky=pygame.image.load(getdir('sky.png'))
+	bird_skin=pygame.transform.scale(pygame.image.load(getdir('bird4.png')),(65,60))	
+	pipe_b_skin = pygame.transform.scale(pygame.image.load(getdir('pipe3.png')),(PIPE_WIDTH,PIPE_HEIGHT))
+	sky=pygame.image.load(getdir('sky2.png'))
 	win=pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
 	pygame.display.set_caption('Flappy Bird')
 	sky=pygame.transform.scale(sky,(WIN_WIDTH,WIN_HEIGHT))
 
-	#Definir variáveis principais	
+	#Variáveis principais
 	pipex = WIN_WIDTH+100
 	pipey_t =rand(-350,0)
 	pipey_b=pipey_t +GAP
@@ -42,28 +50,29 @@ def main():
 	floor_skin = pygame.image.load(getdir('floor.png'))
 	floor_x1,floor_x2,floor_width = -MOVE,-WIN_WIDTH,floor_skin.get_width()
 
-	#loop principal
-	while True:
+
+	#Loop Principal
+	while 1:
 		fps(30)
 		win.blit(sky,(0,0))
 
-		#Key Press
+		#Gravidade
+		birdy +=9
+
+		#Catch Key Press
 		for event in pygame.event.get():
 			if event.type==QUIT:
 				pygame.quit()
 				sys.exit()
 			if event.type==KEYDOWN:
 				if event.key== K_SPACE:
-					birdy -=80
+					birdy -=42
 
-		#Deteta se bird fora da janela ou no chão
-		if birdy>=WIN_HEIGHT-160:
-			break
+		#Deteta se bird no chão ou fora da janela
+		if birdy>=620:break
+		if birdy<-50:break
 
-		if birdy<-50:
-			break
-
-        #Controla o movimento dos tubos e a pontuação
+        #pipe move + score and col
 		pipex-=MOVE
 		if pipex<=-PIPE_WIDTH:
 			pipex=WIN_WIDTH
@@ -73,12 +82,11 @@ def main():
 			pipe_b_collision=pipey_b
 			score+=1
 
-
-		#Desenha tudo
-		win.blit(bird_skin, (birdx,birdy))
-		win.blit(pipe_b_skin,(pipex,pipey_b))
-		win.blit(pipe_t_skin,(pipex,pipey_t))
-		win.blit(floor_skin, (floor_x1,750))
+        #Move Chão
+		floor_x1 -= MOVE 
+		floor_x2 -= MOVE
+		if floor_x1+floor_width<0:floor_x1=floor_x2+floor_width-2
+		if floor_x2+floor_width<0:floor_x2=floor_x1+floor_width-2
 
 		"""desenhos de hitbox
 		pygame.draw.rect(win,(255,0,0)  , pygame.Rect(birdx, birdy,60 , 60)) 
@@ -86,21 +94,24 @@ def main():
 		pygame.draw.rect(win,(0,0,255)  , pygame.Rect(pipex, pipey_b,PIPE_WIDTH ,465))
 		""" 
 
-		#verifica colisao (V1 - Ainda tem algumas falhas sendo possivel bater no cano à saida dele)
+		#verifica colisao (V1 - Ainda tem algumas falhas sendo possiMOVE bater no cano à saida dele)
 		if abs(birdx -pipex)<60:
 			if(birdy<=pipey_t+PIPE_HEIGHT):break
 			if(birdy>=pipey_b-55):break
-
-		#gravidade
-		birdy +=18
 			
-        #trata da score
+		#Desenha tudo
+		win.blit(pipe_b_skin,(pipex,pipey_b))
+		win.blit(pipe_t_skin,(pipex,pipey_t))
+		win.blit(floor_skin, (floor_x1,670))
+		win.blit(floor_skin, (floor_x2,670))
+		win.blit(bird_skin, (birdx,birdy))
+
+        #Write score
 		pygame.font.init()
 		font=pygame.font.SysFont('Arial', 50)
 		text_surface= font.render(f'{score}', False, (255,255,255))
 		win.blit(text_surface,(WIN_WIDTH/2-10,10))
 
-		#atualiza janela
 		pygame.display.update()
 
 main()
